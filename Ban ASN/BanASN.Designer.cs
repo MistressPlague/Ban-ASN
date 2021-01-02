4bcb2d727ea1d6a8231497cd9f87d6c71b16fb9e
Plague <36628963+OFWModz@users.noreply.github.com>
2021-01-02 14:11:45 +0000

    Add files via upload

---
+++ Ban ASN/BanASN.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ban_ASN
+{
+    partial class BanASN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.Magenta;
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ban";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(137, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Un-Ban";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ASN To Ban:";
+            // 
+            // BanASN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(258, 93);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.Magenta;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "BanASN";
+            this.ShowIcon = false;
+            this.Text = "Ban ASN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ban ASN/BanASN.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using Vanara.PInvoke;
+
+namespace Ban_ASN
+{
+    public partial class BanASN : Form
+    {
+        public BanASN()
+        {
+            InitializeComponent();
+        }
+
+        private WebClient client = new WebClient();
+
+        public static FirewallApi.INetFwPolicy2 firewallPolicy = (FirewallApi.INetFwPolicy2)Activator.CreateInstance(
+            Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+
+        public static string ApplyFirewallRule(string ASN, string IP, bool ban)
+        {
+            try
+            {
+                string SanitizedIP = IP.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+
+                string RuleName = "ASN: " + ASN + " Block - IP Count: " + SanitizedIP.Split(',').Length;
+
+                bool Exists = false;
+                FirewallApi.INetFwRule ExistingRule = null;
+
+                foreach (FirewallApi.INetFwRule rule in firewallPolicy.Rules)
+                {
+                    if (rule == null)
+                    {
+                        continue;
+                    }
+
+                    if (rule.Name.Contains("ASN: " + ASN + " Block"))
+                    {
+                        Exists = true;
+                        ExistingRule = rule;
+                        break;
+                    }
+
+                }
+
+                if (ban)
+                {
+                    if (Exists)
+                    {
+                        ExistingRule.Name = RuleName;
+                        ExistingRule.RemoteAddresses = SanitizedIP;
+                        ExistingRule.Description = "Banned ASN: " + ASN + " - IP Count: " + SanitizedIP.Split(',').Length;
+
+                        return "Result: Updated Existing Rule!";
+                    }
+
+                    FirewallApi.INetFwRule firewallRule = (FirewallApi.INetFwRule)Activator.CreateInstance(
+                        Type.GetTypeFromProgID("HNetCfg.FWRule"));
+
+                    firewallRule.Action = FirewallApi.NET_FW_ACTION.NET_FW_ACTION_BLOCK;
+                    firewallRule.Direction = FirewallApi.NET_FW_RULE_DIRECTION.NET_FW_RULE_DIR_IN;
+                    firewallRule.InterfaceTypes = "All";
+
+                    firewallRule.Name = RuleName;
+                    firewallRule.Description = "Banned ASN: " + ASN + " - IP Count: " + SanitizedIP.Split(',').Length;
+                    firewallRule.RemoteAddresses = SanitizedIP;
+
+                    firewallRule.Enabled = true;
+
+                    firewallPolicy.Rules.Add(firewallRule);
+
+                    return "Result: Created Rule!";
+                }
+
+                if (Exists)
+                {
+                    firewallPolicy.Rules.Remove(ExistingRule.Name);
+
+                    return "Result: Removed Rule!";
+                }
+
+                return "Result: Rule Does Not Exist In The First Place!";
+            }
+            catch
+            {
+                return "Result: Failure!";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || !int.TryParse(textBox1.Text, out int test))
+            {
+                MessageBox.Show("You Must Specify An ASN!");
+                return;
+            }
+
+            string[] result = client.DownloadString(
+                "https://www.enjen.net/asn-blocklist/index.php?asn=" + textBox1.Text + "&type=iplist&api=1").Split('\n');
+
+            if (result.Length < 2)
+            {
+                MessageBox.Show("ASN Not Valid!");
+                return;
+            }
+
+            int ruleCount = (int)Math.Max(0, (result.Length - 5000)) + 1;
+
+            for (int i = 0; i < ruleCount; i++)
+            {
+                string ranges = result[((i) * 5000)];
+
+                for (var index = ((i) * 5000); index < result.Length; index++)
+                {
+                    string ip = result[index];
+
+                    if (!string.IsNullOrEmpty(ip) && !ranges.Contains(ip))
+                    {
+                        ranges = ranges + "," + ip;
+                    }
+                }
+
+                if (ranges.Length < 1)
+                {
+                    MessageBox.Show("ASN Not Valid!");
+                    return;
+                }
+
+                MessageBox.Show(ApplyFirewallRule(textBox1.Text + (ruleCount > 1 ? " [" + i + "]" : ""), ranges, true));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text) || !int.TryParse(textBox1.Text, out int test))
+            {
+                MessageBox.Show("You Must Specify An ASN!");
+                return;
+            }
+
+            string[] result = client.DownloadString(
+                "https://www.enjen.net/asn-blocklist/index.php?asn=" + textBox1.Text + "&type=iplist&api=1").Split('\n');
+
+            if (result.Length < 2)
+            {
+                MessageBox.Show("ASN Not Valid!");
+                return;
+            }
+
+            int ruleCount = (int)Math.Max(0, (result.Length - 5000)) + 1;
+
+            for (int i = 0; i < ruleCount; i++)
+            {
+                string ranges = result[((i) * 5000)];
+
+                for (var index = ((i) * 5000); index < result.Length; index++)
+                {
+                    string ip = result[index];
+
+                    if (!string.IsNullOrEmpty(ip) && !ranges.Contains(ip))
+                    {
+                        ranges = ranges + "," + ip;
+                    }
+                }
+
+                if (ranges.Length < 1)
+                {
+                    MessageBox.Show("ASN Not Valid!");
+                    return;
+                }
+
+                MessageBox.Show(ApplyFirewallRule(textBox1.Text + (ruleCount > 1 ? " [" + i + "]" : ""), ranges, false));
+            }
+        }
+    }
+}
